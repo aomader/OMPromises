16141e28f91aa7615335893224323c89d356d7f0
Oliver Mader <b52@reaktor42.de>
2014-03-23 19:53:09 +0000

    Another try

--- Tests/OMPromisesTests/run_tests.sh
+++ Tests/OMPromisesTests/run_tests.sh
@@ -1,2 +1,10 @@
-xctool -workspace OMPromisesTests.xcworkspace -scheme iOS-Tests -sdk iphonesimulator test
-xctool -workspace OMPromisesTests.xcworkspace -scheme OS-X-Tests -sdk macosx test
+#!/bin/sh
+
+xctool -workspace OMPromisesTests.xcworkspace -scheme iOS-Tests -sdk iphonesimulator -configuration Release test ONLY_ACTIVE_ARCH=NO
+ios_tests=$?
+
+xctool -workspace OMPromisesTests.xcworkspace -scheme OS-X-Tests -sdk macosx -configuration Release test ONLY_ACTIVE_ARCH=NO
+osx_tests=$?
+
+exit `expr $ios_tests + $osx_tests`
+
