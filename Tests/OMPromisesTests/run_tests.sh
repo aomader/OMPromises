29870505933a0f5ddb17d9a086fe4456ff641573
Oliver Mader <b52@reaktor42.de>
2014-03-23 20:24:55 +0000

    Assertions throw only in debug

--- Tests/OMPromisesTests/run_tests.sh
+++ Tests/OMPromisesTests/run_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-xctool -workspace OMPromisesTests.xcworkspace -scheme iOS-Tests -sdk iphonesimulator -configuration Release test ONLY_ACTIVE_ARCH=NO
+xctool -workspace OMPromisesTests.xcworkspace -scheme iOS-Tests -sdk iphonesimulator -configuration Debug test ONLY_ACTIVE_ARCH=NO
 ios_tests=$?
 
-xctool -workspace OMPromisesTests.xcworkspace -scheme OS-X-Tests -sdk macosx -configuration Release test ONLY_ACTIVE_ARCH=NO
+xctool -workspace OMPromisesTests.xcworkspace -scheme OS-X-Tests -sdk macosx -configuration Debug test ONLY_ACTIVE_ARCH=NO
 osx_tests=$?
 
 exit `expr $ios_tests + $osx_tests`
