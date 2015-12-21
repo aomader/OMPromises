b3c637bc1bd172e155537cb6bfa43c42b5916315
Oliver Mader <b52@reaktor42.de>
2015-12-21 22:47:11 +0100

    Add support for tvOS
    Running the unit tests by the CI service is currently not possible,
    since `xctool` doesn't support it right now. So, we currently
    only build OMPromises for tvOS and assume that you run the tests
    	locally prior to commiting.

--- Tests/OMPromisesTests/run_tests.sh
+++ Tests/OMPromisesTests/run_tests.sh
@@ -6,5 +6,9 @@ ios_tests=$?
 xctool -workspace OMPromisesTests.xcworkspace -scheme OS-X-Tests -sdk macosx -configuration Debug test ONLY_ACTIVE_ARCH=NO
 osx_tests=$?
 
-exit `expr $ios_tests + $osx_tests`
+# xctool doesnt support tvOS, thus we just built it for now
+xctool -workspace OMPromisesTests.xcworkspace -scheme tvOS-Tests -sdk appletvos -configuration Debug build ONLY_ACTIVE_ARCH=NO
+tvos_tests=$?
+
+exit `expr $ios_tests + $osx_tests + $tvos_tests`
 
